3659aad7ee4d955245b642c17d3479b72ddfbaf2
RakMont <nabogaria@gmail.com>
2024-08-21 22:15:20 -0400

    changes

--- src/router/router.js
+++ src/router/router.js
@@ -5,28 +5,28 @@ import App from "../App"
 import LoginForm from "../screens/forms/LoginForm";
 
 const basename = process.env.NODE_ENV === 'production' ? '/mod7-pr' : '/';
-const routes = createBrowserRouter(
-    [
+
+const routes = createBrowserRouter([
         {
             path:'/',
             element:<App />,
             children:[
-                {
-                path: '/default',
-                element: <Default />,
-                },
-                {
-                    path: '/products',
-                    element: <Product />
-                },
-                {
-                    path: '/forms',
-                    element: <LoginForm />
-                }
-        ]
-    }
-],
-    {
-        basename: basename
-    })
+                    {
+                        path: '/default',
+                        element: <Default />,
+                    },
+                    {
+                        path: '/products',
+                        element: <Product />
+                    },
+                    {
+                        path: '/forms',
+                        element: <LoginForm />
+                    }
+                ]
+        }
+        ],
+            {
+                basename: basename
+            });
 export default routes;
