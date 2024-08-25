73ea72d331aa865f370c7b69f890f76a572140db
RakMont <nabogaria@gmail.com>
2024-08-25 09:40:10 -0400

    fix reload github page

--- src/index.js
+++ src/index.js
@@ -8,17 +8,30 @@ import {Provider} from 'react-redux';
 import router from "./router/router";
 import {RouterProvider} from "react-router-dom";
 
+import { hydrate, render } from "react-dom";
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <Provider store={store}>
           <RouterProvider router={router} />
-
       </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+*/
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+    hydrate(
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>,
+        rootElement)
+} else {
+    render(
+        <Provider store={store}>
+            <RouterProvider router={router}/>
+        </Provider>,
+        rootElement
+    );
+}
+//reportWebVitals();
